802365a8942596884126af27e2d5393083f5b3f1
Emilson-Aguilar <e_aguilar94@hotmail.com>
2026-05-08 19:39:01 -0500

    Importación de librerías

--- Tarea_4.py
+++ Tarea_4.py
@@ -1,1 +1,3 @@
-import tkinter as tk
+from abc import ABC, abstractmethod  # Librería para crear clases abstractas
+import logging  # Librería para manejar logs
+from datetime import datetime  # Librería para fechas
