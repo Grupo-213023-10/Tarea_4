7d5da7e26bac278cba758e956c8a8a1eea15c6bd
Emilson-Aguilar <e_aguilar94@hotmail.com>
2026-05-08 20:08:03 -0500

    se agrega el archivo de logs

--- Tarea_4.py
+++ Tarea_4.py
@@ -1,3 +1,16 @@
 from abc import ABC, abstractmethod  # Librería para crear clases abstractas
 import logging  # Librería para manejar logs
 from datetime import datetime  # Librería para fechas
+
+# Configuración del archivo logs.txt
+logging.basicConfig(
+
+    # Nombre del archivo
+    filename="logs.txt",
+
+    # Nivel de registro
+    level=logging.INFO,
+
+    # Formato del mensaje
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
