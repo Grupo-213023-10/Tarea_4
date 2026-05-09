f9519f9e2eeed11bb8a9ec8cdba5df37ea04f839
Emilson-Aguilar <e_aguilar94@hotmail.com>
2026-05-08 21:06:13 -0500

    Reinicio

--- Tarea_4.py
+++ Tarea_4.py
@@ -2,7 +2,6 @@ from abc import ABC, abstractmethod  # Librería para crear clases abstractas
 import logging  # Librería para manejar logs
 from datetime import datetime  # Librería para fechas
 
-# Configuración del archivo logs.txt
 logging.basicConfig(
 
     # Nombre del archivo
@@ -13,8 +12,4 @@ logging.basicConfig(
 
     # Formato del mensaje
     format="%(asctime)s - %(levelname)s - %(message)s"
-<<<<<<< HEAD
 )
-=======
-)
->>>>>>> 7d5da7e26bac278cba758e956c8a8a1eea15c6bd
